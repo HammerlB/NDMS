d35f3d9714df7f92885ecaa930f3245ed882ebab
Dominik Herkel <dominikherkel@yahoo.de>
2014-10-13 13:54:43 +0200

    Implemented TerminalConnector

--- src/main/java/at/bmlvs/NDMS/domain/connectors/TerminalConnector.java
+++ src/main/java/at/bmlvs/NDMS/domain/connectors/TerminalConnector.java
@@ -2,5 +2,49 @@ package at.bmlvs.NDMS.domain.connectors;
 
 public class TerminalConnector
 {
+	private String host;
+	private String username;
+	private String password;
+	
+	public TerminalConnector()
+	{
+		this("", "", "");
+	}
+	
+	public TerminalConnector(String host, String username, String password)
+	{
+		setHost(host);
+		setUsername(username);
+		setPassword(password);
+	}
 
+	public String getHost()
+	{
+		return host;
+	}
+
+	public void setHost(String host)
+	{
+		this.host = host;
+	}
+
+	public String getUsername()
+	{
+		return username;
+	}
+
+	public void setUsername(String username)
+	{
+		this.username = username;
+	}
+
+	public String getPassword()
+	{
+		return password;
+	}
+
+	public void setPassword(String password)
+	{
+		this.password = password;
+	}
 }
