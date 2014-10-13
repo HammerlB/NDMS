1d1aae20098ba46b60e9f991c254eef60474724f
Dominik Herkel <dominikherkel@yahoo.de>
2014-10-13 14:23:54 +0200

    GUI tabs added

--- src/main/java/at/bmlvs/NDMS/presentation/PresentationFactory.java
+++ src/main/java/at/bmlvs/NDMS/presentation/PresentationFactory.java
@@ -2,5 +2,15 @@ package at.bmlvs.NDMS.presentation;
 
 public class PresentationFactory
 {
+	private static MainWindow mainWindow;
 
+	public static MainWindow getMainWindow()
+	{
+		return mainWindow;
+	}
+
+	public static void setMainWindow(MainWindow mainWindow)
+	{
+		PresentationFactory.mainWindow = mainWindow;
+	}
 }
