e3a3b7cd72d447b28f635a7ca108971fad7560b6
Bernhard Hammerl <hammerlstrash@gmx.at>
2014-10-14 12:31:36 +0200

    Updated GUI Icons ClosedTabbedPane

--- src/main/java/at/bmlvs/NDMS/presentation/MainWindow.java
+++ src/main/java/at/bmlvs/NDMS/presentation/MainWindow.java
@@ -17,6 +17,7 @@ import javax.swing.JPanel;
 import javax.swing.JScrollPane;
 import javax.swing.JTabbedPane;
 import javax.swing.JToolBar;
+import javax.swing.JToolTip;
 import javax.swing.filechooser.FileFilter;
 
 import at.bmlvs.NDMS.domain.templates.Templates;
@@ -48,7 +49,7 @@ public class MainWindow
         bar.add(hilfe);
         
         //Dateimenü
-        JMenuItem neuverb = new JMenuItem("Neue Verbindung");
+        JMenuItem neuverb = new JMenuItem("Neue Verbindung", new ImageIcon("icons/smallnew.png"));
         neuverb.addActionListener(new java.awt.event.ActionListener() {
             public void actionPerformed(java.awt.event.ActionEvent e) {
                 JOptionPane op = new JOptionPane();
@@ -59,19 +60,22 @@ public class MainWindow
             }
         });
             
-        JMenuItem importe = new JMenuItem("Importieren");
+        JMenuItem importe = new JMenuItem("Importieren", new ImageIcon("icons/smallimport.png"));
         importe.addActionListener(new java.awt.event.ActionListener() {
             public void actionPerformed(java.awt.event.ActionEvent e) {
-                JOptionPane op = new JOptionPane();
-                String input = op.showInputDialog("Verbinden zu..");
-                
-                PresentationFactory.getTabbedWindow().addTabbedMenu(input);
+            	load();
                 
             }
         });
-        JMenuItem export = new JMenuItem("Exportieren");
-        JMenuItem einst = new JMenuItem("Einstellungen");
-        JMenuItem exit = new JMenuItem("Beenden");
+        JMenuItem export = new JMenuItem("Exportieren", new ImageIcon("icons/smallexport.png"));
+        JMenuItem einst = new JMenuItem("Einstellungen", new ImageIcon("icons/smallsettings.png"));
+        JMenuItem exit = new JMenuItem("Beenden", new ImageIcon("icons/smallclose.png"));
+        exit.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent e) {
+            	mainFrame.dispose();
+                
+            }
+        });
         
         datei.add(neuverb);
         datei.addSeparator();
@@ -83,26 +87,32 @@ public class MainWindow
         datei.add(exit);
         
         //Konfigmenü
-        JMenuItem portkonf = new JMenuItem("Portkonfiguration");
-        JMenuItem vorlagen = new JMenuItem("Vorlagen");
+        JMenuItem portkonf = new JMenuItem("Port-Konfiguration", new ImageIcon("icons/smallportkonf.png"));
+        JMenuItem vorlagen = new JMenuItem("Vorlagen", new ImageIcon("icons/smallvorlagen.png"));
         
         konfig.add(portkonf);
         konfig.addSeparator();
         konfig.add(vorlagen);
         
         //Werkzeuge
-        JMenuItem tomanage = new JMenuItem("Zur Managementkonfiguration zurücksetzten");
-        JMenuItem tostandart = new JMenuItem("Zur Standartkonfiguration zurücksetzten");
-        JMenuItem diagnose = new JMenuItem("Diagnose");
+        JMenuItem tostandart = new JMenuItem("Zur Standartkonfiguration zurücksetzten", new ImageIcon("icons/smallresetstandart.png"));
+        JMenuItem tomanage = new JMenuItem("Zur Managementkonfiguration zurücksetzten", new ImageIcon("icons/smallresetmanag.png"));
+        JMenuItem diagnose = new JMenuItem("Diagnose", new ImageIcon("icons/smalldiag.png"));
         
-        werkzeug.add(tomanage);
         werkzeug.add(tostandart);
+        werkzeug.add(tomanage);
         werkzeug.addSeparator();
         werkzeug.add(diagnose);
         
         //Hilfe
-        JMenuItem handbook = new JMenuItem("Benutzerhandbuch");
-        JMenuItem about = new JMenuItem("Über NDSM");
+        JMenuItem handbook = new JMenuItem("Benutzerhandbuch", new ImageIcon("icons/smallbook.png"));
+        JMenuItem about = new JMenuItem("Über NDSM", new ImageIcon("icons/smallabout.png"));
+        about.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent e) {
+            	JOptionPane.showMessageDialog(mainFrame,
+            		    "NDMS - Network Device Management Solution\n\nBy Herkel Dominik, Rieppel Alexander und Bernhard Hammerl!\n\nDen glorreichen GWD's der Datenkommunikation!\n\n\nButtons by creative commons\nhttp://creativecommons.org/licenses/by/3.0/\n\nSollten Sie Fehler und Bugs finden ist das Ihr Problem DANKE!");
+            }
+        });
         
         hilfe.add(handbook);
         hilfe.addSeparator();
@@ -116,16 +126,53 @@ public class MainWindow
         JToolBar toolbar1 = new JToolBar();
 
         ImageIcon newi = new ImageIcon("icons/new.png");
-        ImageIcon imp = new ImageIcon("icons/import.png");
-        ImageIcon exp = new ImageIcon("icons/export.png");
+        ImageIcon impi = new ImageIcon("icons/import.png");
+        ImageIcon expi = new ImageIcon("icons/export.png");
+        ImageIcon konfi = new ImageIcon("icons/portkonf.png");
+        ImageIcon vorli = new ImageIcon("icons/vorlagen.png");
+        ImageIcon standi = new ImageIcon("icons/resetstandart.png");
+        ImageIcon diagi = new ImageIcon("icons/diag.png");
+        ImageIcon booki = new ImageIcon("icons/book.png");
 
         JButton newb = new JButton(newi);
-        JButton impb = new JButton(imp);
-        JButton expb = new JButton(exp);
+        JButton impb = new JButton(impi);
+        JButton expb = new JButton(expi);
+        JButton konfb = new JButton(konfi);
+        JButton vorlb = new JButton(vorli);
+        JButton standb = new JButton(standi);
+        JButton diagb = new JButton(diagi);
+        JButton bookb = new JButton(booki);
 
+        newb.setFocusable(false);
+        newb.setToolTipText("Neue Verbindung");
+        impb.setFocusable(false);
+        impb.setToolTipText("Importieren");
+        expb.setFocusable(false);
+        expb.setToolTipText("Exportieren");
+        konfb.setFocusable(false);
+        konfb.setToolTipText("Port-Konfiguration");
+        vorlb.setFocusable(false);
+        vorlb.setToolTipText("Vorlagen");
+        standb.setFocusable(false);
+        standb.setToolTipText("Zurücksetzen zur Standart-Konfiguration");
+        diagb.setFocusable(false);
+        diagb.setToolTipText("Diagnose");
+        bookb.setFocusable(false);
+        bookb.setToolTipText("Hilfe");
+        
         toolbar1.add(newb);
+        toolbar1.addSeparator();
         toolbar1.add(impb);
         toolbar1.add(expb);
+        toolbar1.addSeparator();
+        toolbar1.add(konfb);
+        toolbar1.add(vorlb);
+        toolbar1.addSeparator();
+        toolbar1.add(standb);
+        toolbar1.add(diagb);
+        toolbar1.addSeparator();
+        toolbar1.add(bookb);
+     
         toolbar1.setAlignmentX(0);
         toolbar1.setFloatable(false);
         panel.add(toolbar1);
--- src/main/java/at/bmlvs/NDMS/presentation/TabbedWindow.java
+++ src/main/java/at/bmlvs/NDMS/presentation/TabbedWindow.java
@@ -1,11 +1,17 @@
 package at.bmlvs.NDMS.presentation;
 
+import java.awt.GridBagConstraints;
+import java.awt.GridBagLayout;
+
+import javax.swing.JButton;
+import javax.swing.JLabel;
 import javax.swing.JPanel;
 import javax.swing.JTabbedPane;
 
 public class TabbedWindow
 {
 	JTabbedPane tabpane = new JTabbedPane(JTabbedPane.TOP,JTabbedPane.SCROLL_TAB_LAYOUT);
+	int index;
 	
 	public TabbedWindow()
 	{
@@ -14,7 +20,32 @@ public class TabbedWindow
 
 	public void addTabbedMenu(String tabname)
 	{
+		
 		tabpane.addTab(tabname,new JPanel());
+
 		PresentationFactory.getMainWindow().mainFrame.add(tabpane);
+		
+		
+		//closebutton
+		index = tabpane.indexOfTab(tabname);
+		JPanel pnlTab = new JPanel(new GridBagLayout());
+		pnlTab.setOpaque(false);
+		JLabel lblTitle = new JLabel(tabname + "  ");
+		JButton btnClose = new JButton("x");
+
+		GridBagConstraints gbc = new GridBagConstraints();
+		gbc.gridx = 0;
+		gbc.gridy = 0;
+		gbc.weightx = 1;
+
+		pnlTab.add(lblTitle, gbc);
+
+		gbc.gridx++;
+		gbc.weightx = 0;
+		pnlTab.add(btnClose, gbc);
+
+		tabpane.setTabComponentAt(index, pnlTab);
+
+		//btnClose.addActionListener(myCloseActionHandler);
 	}
 }
