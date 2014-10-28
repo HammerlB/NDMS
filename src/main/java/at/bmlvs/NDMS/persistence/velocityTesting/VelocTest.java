8493b7576009f66247e623e1a5e7d848b20fd530
Alexander Rieppel <arieppel@gmail.com>
2014-10-28 10:31:23 +0100

    Simple Velocity example

---
+++ src/main/java/at/bmlvs/NDMS/persistence/velocityTesting/VelocTest.java
@@ -0,0 +1,24 @@
+package at.bmlvs.NDMS.persistence.velocityTesting;
+
+import java.io.StringWriter;
+import org.apache.velocity.app.VelocityEngine;
+import org.apache.velocity.Template;
+import org.apache.velocity.VelocityContext;
+
+public class VelocTest {
+	public static void main(String[] args) throws Exception {
+		/* first, get and initialize an engine */
+		VelocityEngine ve = new VelocityEngine();
+		ve.init();
+		/* next, get the Template */
+		Template t = ve.getTemplate("hello.vm");
+		/* create a context and add data */
+		VelocityContext context = new VelocityContext();
+		context.put("name", "World");
+		/* now render the template into a StringWriter */
+		StringWriter writer = new StringWriter();
+		t.merge(context, writer);
+		/* show the World */
+		System.out.println(writer.toString());
+	}
+}
