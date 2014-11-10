17380304055252d7d2baa25d32f998fa81895342
Alexander Rieppel <arieppel@gmail.com>
2014-11-10 15:47:58 +0100

    TFTP startet

--- src/main/java/at/bmlvs/NDMS/domain/connectors/TFTPConnector.java
+++ src/main/java/at/bmlvs/NDMS/domain/connectors/TFTPConnector.java
@@ -1,13 +1,27 @@
 package at.bmlvs.NDMS.domain.connectors;
 
-public class TFTPConnector extends Thread{
-	
-	public TFTPConnector(String host, String localFile, String remoteFile){
-		
+import at.bmlvs.NDMS.service.ServiceFactory;
+
+public class TFTPConnector extends Thread {
+
+	private String host, localFile, remoteFile;
+	private ConnectionTFTP tftp;
+
+	public TFTPConnector(String host, String localFile, String remoteFile) {
+		this.host = host;
+		this.localFile = localFile;
 	}
 
 	public void run() {
-		
+		while(ServiceFactory.getDomainService().getInstances().getInstances().get(0).getSshConnector().isConnected()==false){
+			try {
+				sleep(5000);
+			} catch (InterruptedException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+		}
+		System.out.println("SSH Connected!");
 	}
 
 }
