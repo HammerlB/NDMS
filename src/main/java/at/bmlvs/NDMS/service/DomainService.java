a9e0f2e13aee4c0d81faa92c77fc691b7db54795
Dominik Herkel <dominikherkel@yahoo.de>
2014-11-10 10:30:06 +0100

    Changed DomainService

--- src/main/java/at/bmlvs/NDMS/service/DomainService.java
+++ src/main/java/at/bmlvs/NDMS/service/DomainService.java
@@ -1,10 +1,12 @@
 package at.bmlvs.NDMS.service;
 
 import at.bmlvs.NDMS.domain.Instances;
+import at.bmlvs.NDMS.domain.templates.Templates;
 
 public class DomainService
 {
 	private static Instances instances;
+	private static Templates templates;
 
 	public static Instances getInstances()
 	{
@@ -14,5 +16,15 @@ public class DomainService
 	public static void setInstances(Instances instances)
 	{
 		DomainService.instances = instances;
+	}
+
+	public static Templates getTemplates()
+	{
+		return templates;
+	}
+
+	public static void setTemplates(Templates templates)
+	{
+		DomainService.templates = templates;
 	}
 }
